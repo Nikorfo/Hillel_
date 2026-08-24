9cbc3a93da82bbde27eabe351cd2cc79268c35ee
Mykola <nikorfo1@gmail.com>
2026-08-24 18:42:55 +0300

    Revert "Homework 1"

--- home_work1.py
+++
@@ -1,6 +0,0 @@
-
-numbers = input("Введіть список цілих чисел через пробіл: ")
-list = list(map(int, numbers.split()))
-max_value = max(list)
-max_index = list.index(max_value)
-print(max_value, max_index)
